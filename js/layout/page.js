a01481a17b3bddca7fc9047c44c4c59ddf5cbaa1
Tyler Childs <tylerwchilds@gmail.com>
2017-10-08 11:39:54 -0700

    javascript layout axed

--- js/layout/page.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  document.getElementById('menu-toggle').addEventListener('click', function(e) {
-    [].map.call(document.querySelectorAll('.sidebar'), function(el) {
-
-      el.classList.toggle('active');
-    });
-  });
-})();
--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 (function(){
   var $print = document.querySelectorAll('.js-print');
+  var $menuToggle = document.querySelectorAll('#menu-toggle');
 
   $print.forEach(function(el){
     el.addEventListener('click', window.print);
+  })
+
+  $menuToggle.forEach(function(el){
+    el.addEventListener('click', function(e) {
+      [].map.call(document.querySelectorAll('.sidebar'), function(el) {
+
+        el.classList.toggle('active');
+      });
+    });
   })
 })();
